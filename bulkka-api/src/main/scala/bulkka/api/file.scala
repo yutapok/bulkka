915d280c58126533a444af8b00ff2b4a4645af17
yutapok <yutautsugi@outlook.com>
2021-03-07 00:29:56 +0900

    modify: add Out to file

--- bulkka-api/src/main/scala/bulkka/api/file.scala
+++ bulkka-api/src/main/scala/bulkka/api/file.scala
@@ -1,6 +1,6 @@
 package bulkka.api
 
-import akka.NotUsed
+import akka.{NotUsed, Done}
 import akka.stream.scaladsl._
 import akka.util.ByteString
 
@@ -10,9 +10,42 @@ import scala.util.Try
 import java.io._
 import java.util.zip._
 
+
+sealed abstract class FILE_TYPE(val str:String){
+  def toStr: String = str
+}
+case object FT_JSON extends FILE_TYPE("json")
+case object FT_CSV extends FILE_TYPE("csv")
+case object FT_PLAIN extends FILE_TYPE("txt")
+
+sealed abstract class ENCODE(val str:String){
+  def toStr: String = str
+}
+case object ENC_UTF8 extends ENCODE("UTF-8")
+
+sealed abstract class TERMINATION(val str: String){
+  def toStr = str
+}
+case object LF extends TERMINATION("\n")
+case object CRLF extends TERMINATION("\r\n")
+
 case class InFile(content: String){
   def toRows: Seq[String] = content.split("\n").map(row => row.replace("\r", ""))
 }
+
+case class OutFile[T](
+  name: String,
+  path: String,
+  ftype: FILE_TYPE,
+  contents: Seq[T],
+  encode: ENCODE,
+  termination: TERMINATION,
+  append: Boolean
+){
+  def toFileStr = contents.mkString(termination.toStr) + termination.toStr
+  def toFilePath = new File(s"/${path}/${name}.${ftype.toStr}")
+}
+
 
 class Decompress extends BulkkaTemplate[Seq[Array[Byte]], Array[Byte], InFile, Seq[InFile]]
   with MixinImplBase {
@@ -52,6 +85,27 @@ class InFileRows extends BulkkaTemplate[Seq[InFile], InFile, Seq[String], Seq[St
       Sink.fold(Seq.empty[String])((acc, seq) => acc ++ seq)
 }
 
+class OutFileRows extends BulkkaTemplate[Seq[OutFile[String]], OutFile[String], (OutputStreamWriter, String), Done]
+  with MixinImplBase {
+    override def toTaked(in: Seq[OutFile[String]]): Source[OutFile[String], NotUsed] =
+      Source.fromIterator(() => in.toIterator)
+
+    override def toTakedFuture(inF: Future[Seq[OutFile[String]]]): Source[OutFile[String], NotUsed] = {
+      val srcF = Source.future(inF)
+      srcF.flatMapConcat(seq => Source.fromIterator(() => seq.toIterator))
+    }
+
+    override def toTransformed: Flow[OutFile[String], (OutputStreamWriter, String), NotUsed] =
+      Flow[OutFile[String]].map(of => {
+        val fileOutPutStream = new FileOutputStream(of.toFilePath, of.append)
+        val writer = new OutputStreamWriter(fileOutPutStream, of.encode.toStr)
+        (writer, of.toFileStr)
+      })
+
+    override def toFolded: Sink[(OutputStreamWriter, String), Future[Done]] =
+      Sink.foreach{case((w, s)) => implOutF.writeStr(w, s)}
+}
+
 class ImplInFile {
   def convertRows(fileData: InFile): Seq[String] = fileData.toRows
   def decompressGz(data: Array[Byte]): Option[String] = {
@@ -60,6 +114,13 @@ class ImplInFile {
       gzis <- Try(new GZIPInputStream(bais))
     } yield scala.io.Source.fromInputStream(gzis).mkString)
       .toOption
+  }
+}
+
+class ImplOutFile {
+  def writeStr(writer: OutputStreamWriter, rowStr: String){
+    writer.write(rowStr)
+    writer.close()
   }
 }
 
@@ -84,18 +145,32 @@ class BulkkaEngineFile {
       InFileRows
     ](InFileRows, inOpt, inFOpt)
   }
+
+  def runGraphOutFileRows(inOpt: Option[Seq[OutFile[String]]], inFOpt: Option[Future[Seq[OutFile[String]]]]): Future[Done] = {
+    engine.runGraph[
+      Seq[OutFile[String]],
+      OutFile[String],
+      (OutputStreamWriter, String),
+      Done,
+      OutFileRows
+    ](OutFileRows, inOpt, inFOpt)
+  }
 }
 
 
 object Decompress extends Decompress
 object InFileRows extends InFileRows
+object OutFileRows extends OutFileRows
 object ImplInFile extends ImplInFile
+object ImplOutFile extends ImplOutFile
 object BulkkaEngineFile extends BulkkaEngineFile
 
-trait UseImplDecompress {
+trait UseImplBase {
     val implInF: ImplInFile
+    val implOutF: ImplOutFile
 }
 
-trait MixinImplBase extends UseImplDecompress {
+trait MixinImplBase extends UseImplBase{
     override val implInF: ImplInFile = ImplInFile
+    override val implOutF: ImplOutFile = ImplOutFile
 }
